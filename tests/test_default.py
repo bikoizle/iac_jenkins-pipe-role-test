c91648779203b1a15f067c537512be66afe52c2b
bikoizle <cmartinoo@uoc.edu>
2019-12-13 19:47:20 +0100

    Remade tests

--- tests/test_default.py
+++ tests/test_default.py
@@ -1,11 +1,3 @@
-import pytest
-
-
 def test_os_release_file(host):
     f = host.file('/etc/os-release')
     assert f.exists
-
-
-def test_firewalld_service_is_active_and_enabled(host):
-    assert host.service("firewalld").is_running
-    assert host.service("firewalld").is_enabled
